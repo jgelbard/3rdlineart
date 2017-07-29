845ff009a6725b2a50a9fd9b6262f8addfb55e16
Dumisani Ndhlovu <j.dumisani7291@gmail.com>
2017-07-29 09:52:45 +0200

    updated the admin nav_sub, moving buttons to the sub nav

--- admin/includes/nav_sub.php
+++ admin/includes/nav_sub.php
@@ -2,7 +2,19 @@
   <div class="subnavbar-inner">
     <div class="container">
       <ul class="mainnav">
-        <li><a href="dash.php?p"><img src="img/app_log.png" width="100px"></a></li> 
+        <li><a href="dash.php?p"><img src="../img/app_log.png" width="100px"></a></li> 
+        <li class="active"><a href="dash.php?man_facility" class="button btn btn-invert btn-large"  style="margin:2px">Facility </a></li> 
+        <li><a href="dash.php?man_drugs" class="button btn btn-invert btn-large" style="margin:2px">Drugs </a></li>
+        <li><a href="dash.php?man_affliates" class="button btn btn-invert btn-large" style="margin:2px">Affliates </a></li>
+        <li><a href="dash.php?rev" class="button btn btn-invert btn-large" style="margin:2px">Reviewers </a></li>
+        <li><a href="dash.php?clin" class="button btn btn-invert btn-large" style="margin:2px">Clinician </a></li>           
+        <li><a href="dash.php?lab" class="button btn btn-invert btn-large" style="margin:2px">Lab user </a></li>
+        <li><a href="dash.php?sec" class="button btn btn-invert btn-large" style="margin:2px">Secretary </a></li>
+
+                 
+        <li><a href="../reports.php" target="_blank" class="button btn btn-invert btn-large" style="margin:2px">Reports </a> </li>
+
+          
       </ul>
     </div>
   </div>
